b3118f1a4401a17d9e25ba100c64413f50376b63
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2025-05-25 22:01:52 +0300

    Add == and * operators to Vector2D

--- vector2d.cpp
+++ vector2d.cpp
@@ -8,6 +8,12 @@ public:
         : x_(x), y_(y) {}
     double x() const { return x_; }
     double y() const { return y_; }
+
+    friend bool operator==(const Vector2D a, const Vector2D b) {
+        return a.x() == b.x() && a.y() == b.y();
+    }
+
+    Vector2D operator*(double c) const { return Vector2D(x_ * c, y_ * c); }
     double length() const { return std::sqrt(x_ * x_ + y_ * y_); }
 
 private:
@@ -28,4 +34,12 @@ TEST_CASE("Length")
 {
     Vector2D v = {3, 4};
     CHECK_EQ(v.length(), 5.0);
+}
+
+
+TEST_CASE("Multiply by scalar")
+{
+    Vector2D v = {-1, 2};
+    Vector2D expected = {-3, 6};
+    CHECK_EQ(v * 3, expected);
 }
