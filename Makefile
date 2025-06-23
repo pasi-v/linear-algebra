ef17008c28c107b33a33b03739aaa895f597a4af
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2025-06-23 17:52:19 +0300

    Add Vector3D and crossProduct

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXX      := clang++
 CXXFLAGS := -std=c++11 -Wall -Wextra -g
 
 # Source and target
-SRC      := test_main.cpp vector.cpp vector2d.cpp test_math_utils.cpp
+SRC      := test_main.cpp vector.cpp vector2d.cpp vector3d.cpp test_math_utils.cpp
 HEADERS  := math_utils.h
 TARGET   := tests
 
---
+++ vector3d.cpp
@@ -0,0 +1,35 @@
+#include "doctest.h"
+
+
+class Vector3D {
+  public:
+    Vector3D(const double x, const double y, const double z) : x_(x), y_(y), z_(z) {}
+
+    double x() const { return x_; }
+    double y() const { return y_; }
+    double z() const { return z_; }
+
+    friend bool operator==(const Vector3D a, const Vector3D b) {
+        return a.x() == b.x() && a.y() == b.y() && a.z() == b.z();
+    }
+
+    Vector3D crossProduct(Vector3D v) const {
+        return Vector3D(y() * v.z() - z() * v.y(),
+                        z() * v.x() - x() * v.z(),
+                        x() * v.y() - y() * v.x());
+    }
+
+  private:
+    double x_;
+    double y_;
+    double z_;
+};
+
+
+TEST_CASE("Cross product") {
+    Vector3D u = {0, 1, 1};
+    Vector3D v = {3, -1, 2};
+    Vector3D expected = {3, 3, -3};
+    Vector3D n = u.crossProduct(v);
+    CHECK_EQ(n, expected);
+}
