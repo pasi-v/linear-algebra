ee799c4539277a7536daaa545a628609a8f8d250
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2025-08-18 19:11:35 +0300

    parity_bit()

--- parity.cpp
+++ parity.cpp
@@ -13,6 +13,17 @@ template <std::size_t N> bool has_parity_error(const std::bitset<N> &bits) {
     return (bits.count() % 2) != 0;
 }
 
+/**
+ * Calculate the parity bit for the bitset.  The parity bit will make
+ * the number of bits in the bitset even.
+ *
+ * @param bits the bitset without the parity bit.
+ * @return true if the parity bit should be 1, false if 0
+ */
+template <std::size_t N> bool parity_bit(const std::bitset<N> &bits) {
+    return (bits.count() % 2) != 0;
+}
+
 TEST_CASE("1010") {
     std::bitset<4> bits("1010");
     CHECK(!has_parity_error(bits));
@@ -31,4 +42,14 @@ TEST_CASE("010111") {
 TEST_CASE("11010111") {
     std::bitset<8> bits("11010111");
     CHECK(!has_parity_error(bits));
+}
+
+TEST_CASE("Parity bit for 1011") {
+    std::bitset<4> bits("1011");
+    CHECK(parity_bit(bits) == true);
+}
+
+TEST_CASE("Parity bit for 11011") {
+    std::bitset<5> bits("11011");
+    CHECK(parity_bit(bits) == false);
 }
