b74a80ff6bbed48bf2e20af44dfb91831c18784e
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2025-12-21 13:23:10 +0200

    tests/test_utils.hpp

---
+++ tests/test_utils.hpp
@@ -0,0 +1,10 @@
+#ifndef TEST_UTILS_HPP
+#define TEST_UTILS_HPP
+
+constexpr double kTestAbsTol = 1e-12;
+constexpr double kTestRelTol = 1e-10;
+
+#define CHECK_VEC_NEAR(a, b) \
+CHECK(la::approx_equal(a, b, kTestAbsTol, kTestRelTol))
+
+#endif // TEST_UTILS_HPP
