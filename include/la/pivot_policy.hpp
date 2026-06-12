bae3dab4fbc4a4b8533112a6426ddd12e401685f
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2026-06-12 21:05:44 +0300

    Remove is_effectively_zero_pivot
    Was not needed, the checks in row reduction handle this differently.

--- include/la/pivot_policy.hpp
+++ include/la/pivot_policy.hpp
@@ -6,14 +6,9 @@
 namespace la {
 
 constexpr double kPivotAbsTol = 1e-12;
-constexpr double kPivotRelTol = 1e-10;
 
 inline bool is_zero_pivot(double x) {
     return math_utils::is_near_zero_abs(x, kPivotAbsTol);
-}
-
-inline bool is_effectively_zero_pivot(double x, double scale) {
-    return std::fabs(x) <= kPivotAbsTol + kPivotRelTol * scale;
 }
 
 } // namespace la
